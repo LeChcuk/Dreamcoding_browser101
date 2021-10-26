5ea2c78310bca04619d9fdc29789e813ce92f15a
pjw9412 <pjwts9412@gmail.com>
2021-10-27 01:17:45 +0900

    1'

--- 07_shoppingApp(DOM)/main.js
+++ 07_shoppingApp(DOM)/main.js
@@ -1,12 +1,25 @@
+// 엔터 입력 시 목록 추가
 const inputBox = document.querySelector('.listWindow__textInput input');
 inputBox.addEventListener('keypress', (e) => {
     if (e.key === 'Enter') {
         // 리스트 추가 함수
-        console.log('hi');
+        console.log('Enterd');
+        addList();
     }
 });
 
+// + 버튼 클릭 시 목록 추가
 const addBtn = document.querySelector('.listWindow__footer .footer__circle');
 addBtn.addEventListener('click', () => {
     console.log('clicked');
+    addList();
 });
+
+// 리스트 추가 함숨
+function addList() {
+    const blockEle = document.createelement('div');
+    const txtEle = document.createElement('span');
+}
+
+// DELETE
+// 목록 내 휴지통 버튼 클릭 시 제거
