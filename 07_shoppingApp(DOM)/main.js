789ac346d5574882ccd8c62fd0f0f96535212880
pjw9412 <pjwts9412@gmail.com>
2021-11-13 16:41:33 +0900

    완료

--- 07_shoppingApp(DOM)/main.js
+++ 07_shoppingApp(DOM)/main.js
@@ -3,7 +3,6 @@ const inputBox = document.querySelector('.listWindow__textInput input');
 inputBox.addEventListener('keypress', (e) => {
     if (e.key === 'Enter') {
         // 리스트 추가 함수
-        console.log('Enterd');
         addList();
     }
 });
@@ -11,15 +10,34 @@ inputBox.addEventListener('keypress', (e) => {
 // + 버튼 클릭 시 목록 추가
 const addBtn = document.querySelector('.listWindow__footer .footer__circle');
 addBtn.addEventListener('click', () => {
-    console.log('clicked');
     addList();
 });
 
-// 리스트 추가 함숨
+// 리스트 추가 함수
+const contentList = document.querySelector('.listWindow__content');
 function addList() {
-    const blockEle = document.createelement('div');
+    const blockEle = document.createElement('div');
+
     const txtEle = document.createElement('span');
+    blockEle.appendChild(txtEle);
+
+    const txtNode = document.createTextNode(inputBox.value);
+    txtEle.appendChild(txtNode);
+
+    const iconEle = document.createElement('i');
+    iconEle.setAttribute('class', 'fas fa-trash');
+    blockEle.appendChild(iconEle);
+
+    contentList.appendChild(blockEle);
+
+    inputBox.value = '';
 }
 
 // DELETE
 // 목록 내 휴지통 버튼 클릭 시 제거
+const delBtn = document.querySelectorAll('.listWindow__content i');
+delBtn.forEach((n) => {
+    n.addEventListener('click', () => {
+        contentList.removeChild(n.parentNode);
+    });
+});
