b7c92db5a9328eeb74d358494e7ea17ebaffa8b0
pjw9412 <pjwts9412@gmail.com>
2022-02-14 15:17:08 +0900

    trying to show up bugs and carrots

--- 09_CarrotGame/main.js
+++ 09_CarrotGame/main.js
@@ -1,39 +1,13 @@
 const gameField = document.querySelector('.game__field');
+const fieldRect = gameField.getBoundingClientRect();
 
-const gameBtn = document.querySelector('.game__button');
-gameBtn.addEventListener('click', (e) => {
-    init();
-});
-
-function init() {
-    console.log('게임 시작!');
-    // Bug를 .game__field에 랜덤으로 5개 포지셔닝
-    // Carrot을 .game__field에 랜덤으로 5개 포지셔닝
-
-    makeBugAndCarrot(5);
+function initGame() {
+    // 벌레와 당근을 생성한 뒤 field에 추가해줌
+    console.log(fieldRect);
+    addItem('carrot', 5, '/img/carrot.png');
+    addItem('bug', 5, 'img/bug.png');
 }
 
-function makeBugAndCarrot(num) {
-    const container = document.createDocumentFragment();
+function addItem(className, count, imgPath) {}
 
-    for (let i = 0; i < num; i++) {
-        let bug = document.createElement('span');
-        let carrot = document.createElement('span');
-        // setAttribute와 classList의 차이?
-        // setAttribute('class')로 여러 개를 set할 순 없는지??
-        bug.classList.add('bug');
-        bug.classList.add('objects');
-        carrot.setAttribute('class', 'carrot');
-        carrot.classList.add('objects');
-
-        bug.style.top = Math.floor(Math.random() * (700 - 0)) + 'px';
-        bug.style.left = Math.floor(Math.random() * (2000 - 0)) + 'px';
-        carrot.style.top = Math.floor(Math.random() * (700 - 0)) + 'px';
-        carrot.style.left = Math.floor(Math.random() * (2000 - 0)) + 'px';
-
-        container.appendChild(bug);
-        container.appendChild(carrot);
-    }
-
-    gameField.appendChild(container);
-}
+initGame();
