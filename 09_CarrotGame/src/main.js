e6dd9eb3e9c73f6be5f57bd3a0cd0bb9d4d7c003
pjw9412 <pjwts9412@gmail.com>
2022-02-15 14:33:10 +0900

    beforeeee game.js

--- 09_CarrotGame/src/main.js
+++ 09_CarrotGame/src/main.js
@@ -1,93 +1,86 @@
 'use strict';
 
-import PopUp from './popup.js';
-import Field from './field.js';
-import Game from './game.js';
-import * as sound from './sound.js';
+const CARROT_SIZE = 80;
+const CARROT_COUNT = 5;
+const BUG_COUNT = 5;
+const GAME_DURATION_SEC = 5;
 
-const CARROT_COUNT = 20;
-const BUG_COUNT = 20;
-const GAME_DURATION_SEC = 20;
+const field = document.querySelector('.game__field');
+const fieldRect = field.getBoundingClientRect();
+const gameBtn = document.querySelector('.game__button');
+const gameTimer = document.querySelector('.game__timer');
+const gameScore = document.querySelector('.game__score');
 
-const timerIndicator = document.querySelector('.game__timer');
-// const gameScore = document.querySelector('.game__score');
+const popUp = document.querySelector('.pop-up');
+const popUpMessage = document.querySelector('.pop-up__message');
+const popUpRefresh = document.querySelector('.pop-up__refresh');
 
-// let started = false;
-// let score = 0;
+let started = false;
+let score = 0;
 let timer = undefined;
 
-const gameFinishBanner = new PopUp();
-gameFinishBanner.setClickListener(() => {
-    startGame();
+field.addEventListener('click', ({ target }) => {
+    if (!target.matches('img')) return;
+    else if (target.matches('img.bug')) {
+        lostGame();
+    } else {
+        console.log('carrot here');
+        target.parentNode.removeChild(target);
+        score++;
+
+        if (score === CARROT_COUNT) {
+            hideGameButton();
+            stopGameTimer();
+            showPopUpWithText('YOU WON🎆');
+        }
+    }
 });
 
-const gameField = new Field(CARROT_COUNT, BUG_COUNT);
-gameField.setClickListener(onItemClick);
+popUpRefresh.addEventListener('click', () => {
+    replayGame();
+});
 
-function onItemClick(item) {
-    if (!started) return;
-    if (item === 'carrot') {
-        score++;
-        updateScoreBoard();
-        if (score === CARROT_COUNT) finishGame(true);
-    } else if (item === 'bug') finishGame(false);
+function replayGame() {
+    showGameButton();
+    startGameTimer();
+    hidePopUp();
+    initGame();
+    started = true;
 }
 
-const game = new Game();
-
-// gameBtn.addEventListener('click', () => {
-//     if (started) {
-//         stopGame();
-//     } else {
-//         startGame();
-//     }
-// });
-
-// function startGame() {
-//     started = true;
-//     initGame();
-//     showStopButton();
-//     showTimerAndScore();
-//     startGameTimer();
-//     sound.playBackground();
-// }
-
-// function stopGame() {
-//     started = false;
-//     stopGameTimer();
-//     hideGameButton();
-//     gameFinishBanner.showWithText('REPLAY❓');
-//     sound.playAlert();
-//     sound.stopBackground();
-// }
-
-function finishGame(win) {
-    started = false;
-    hideGameButton();
-    if (win) {
-        sound.playWin();
+gameBtn.addEventListener('click', () => {
+    if (started) {
+        stopGame();
     } else {
-        sound.playBug();
+        startGame();
     }
+    started = !started;
+});
+
+function startGame() {
+    initGame();
+    showStopButton();
+    showTimerAndScore();
+    startGameTimer();
+}
+function stopGame() {
     stopGameTimer();
-    sound.stopBackground();
-    gameFinishBanner.showWithText(win ? 'YOU WON 🎉' : 'YOU LOST 💩');
+    hideGameButton();
+    showPopUpWithText('REPLAY?');
 }
 
-// function showStopButton() {
-//     const icon = gameBtn.querySelector('.fas');
-//     icon.classList.add('fa-stop');
-//     icon.classList.remove('fa-play');
-//     gameBtn.style.visibility = 'visible';
-// }
+function showStopButton() {
+    const icon = gameBtn.querySelector('.fa-solid');
+    icon.classList.add('fa-stop');
+    icon.classList.remove('fa-play');
+}
 
 function hideGameButton() {
     gameBtn.style.visibility = 'hidden';
 }
 
-function showTimerAndScore() {
-    timerIndicator.style.visibility = 'visible';
-    gameScore.style.visibility = 'visible';
+function showGameButton() {
+    gameBtn.style.visibility = 'visible';
 }
 
 function lostGame() {
@@ -101,8 +94,7 @@ function startGameTimer() {
     updateTimerText(remainingTimeSec);
     timer = setInterval(() => {
         if (remainingTimeSec <= 0) {
-            clearInterval(timer);
-            finishGame(score === CARROT_COUNT);
+            lostGame();
             return;
         }
         updateTimerText(--remainingTimeSec);
@@ -116,15 +108,50 @@ function stopGameTimer() {
 function updateTimerText(time) {
     const minutes = Math.floor(time / 60);
     const seconds = time % 60;
-    timerIndicator.innerHTML = `${minutes}:${seconds}`;
+    gameTimer.textContent = `${minutes}:${seconds}`;
 }
 
-// function initGame() {
-//     score = 0;
-//     gameScore.innerText = CARROT_COUNT;
-//     gameField.init();
-// }
+function showTimerAndScore() {
+    gameTimer.style.visibility = 'visible';
+    gameScore.style.visibility = 'visible';
+}
 
-function updateScoreBoard() {
-    gameScore.innerText = CARROT_COUNT - score;
+function showPopUpWithText(text) {
+    popUpMessage.innerHTML = text;
+    popUp.classList.remove('pop-up--hide');
+}
+
+function hidePopUp() {
+    popUp.classList.add('pop-up--hide');
+}
+
+function initGame() {
+    field.innerHTML = '';
+    gameScore.innerText = CARROT_COUNT;
+    // 벌레와 당근을 생성한 뒤 field에 추가해줌
+    addItem('carrot', CARROT_COUNT, 'img/carrot.png');
+    addItem('bug', BUG_COUNT, 'img/bug.png');
+}
+
+function addItem(className, count, imgPath) {
+    const x1 = 0;
+    const y1 = 0;
+    const x2 = fieldRect.width - CARROT_SIZE;
+    const y2 = fieldRect.height - CARROT_SIZE;
+
+    for (let i = 0; i < count; i++) {
+        const item = document.createElement('img');
+        item.setAttribute('class', className);
+        item.setAttribute('src', imgPath);
+        item.style.position = 'absolute';
+        const x = randomNumber(x1, x2);
+        const y = randomNumber(y1, y2);
+        item.style.left = `${x}px`;
+        item.style.top = `${y}px`;
+        field.appendChild(item);
+    }
+}
+
+function randomNumber(min, max) {
+    return Math.random() * (max - min) + min;
 }
