ae02c83db9733afb044b53fb9974e53192fc4b5a
pjw9412 <pjwts9412@gmail.com>
2022-02-18 22:28:39 +0900

    1

--- extra_shop/src/main.js
+++ extra_shop/src/main.js
@@ -1,110 +1,50 @@
-// 1) color - blue/yellow/pink
-// 2) 종류 - T-shirt/pants/skirts (t,s,p)
-// 3) man, female
-// 4) large or small
-
-const bodyMain = document.querySelector('.body__main');
-const filterDiv = document.querySelector('.body__filterBtnsDiv');
-const bodyLogo = document.querySelector('.body__logo');
-
-const tshirftsFilter = document.querySelector('.body__shirts');
-const pantsFilter = document.querySelector('.body__pants');
-const skirtsFilter = document.querySelector('.body__skirts');
-const blueFilter = document.querySelector('.body__blue');
-const yellowFilter = document.querySelector('.body__yellow');
-const pinkFilter = document.querySelector('.body__pink');
-
-let imgs;
-
-bodyLogo.addEventListener('click', () => {
-    bodyMain.innerHTML = '';
-    makeCards(15);
-});
-
-filterDiv.addEventListener('click', ({ target }) => {
-    if (target.matches('.body__shirts')) filteringCards('_t');
-    else if (target.matches('.body__pants')) filteringCards('_p');
-    else if (target.matches('.body__skirts')) filteringCards('_s');
-    else if (target.matches('.body__blue')) filteringCards('blue');
-    else if (target.matches('.body__yellow')) filteringCards('yellow');
-    else filteringCards('pink');
-});
-
-function filteringCards(str) {
-    imgs.forEach((img) => {
-        img.parentElement.style.display = 'flex';
-        if (!img.getAttribute('src').includes(str)) img.parentElement.style.display = 'none';
-    });
+// Fetch the items from the JSON file
+function loadItems() {
+    return fetch('./data/data.json')
+        .then((response) => response.json())
+        .then((json) => json.items);
 }
 
-function makeCards(num) {
-    const container = document.createDocumentFragment();
-    let cardDiv, cardImg, cardText;
-
-    for (let i = 0; i < num; i++) {
-        cardDiv = document.createElement('div');
-        cardImg = document.createElement('img');
-        cardText = document.createElement('p');
-
-        cardDiv.setAttribute('class', 'main__card');
-        cardImg.setAttribute('src', `./img/${makeRandomImg(3)}`); // 랜덤으로 지정
-        cardImg.setAttribute('class', 'main__imgInfo');
-        cardText.setAttribute('class', 'main__textInfo');
-
-        // 랜덤으로 cardText innerHTMl 내용 지정
-        // (${female or male}, ${large or small} size)
-        cardText.textContent = makeRandomText();
-
-        cardDiv.appendChild(cardImg);
-        cardDiv.appendChild(cardText);
-        container.appendChild(cardDiv);
-    }
-
-    bodyMain.appendChild(container);
-    imgs = document.querySelectorAll('.main__card img');
+// Update the list with the given items
+function displayItems(items) {
+    const container = document.querySelector('.items');
+    container.innerHTML = items.map((item) => createHTMLString(item)).join('');
 }
 
-function makeRandomImg() {
-    let color, wear;
-
-    switch (getRandom(3)) {
-        case 0:
-            color = 'blue';
-            break;
-        case 1:
-            color = 'yellow';
-            break;
-        case 2:
-            color = 'pink';
-            break;
-        default:
-            throw new Error('what?!');
-    }
-
-    switch (getRandom(3)) {
-        case 0:
-            wear = 'p';
-            break;
-        case 1:
-            wear = 's';
-            break;
-        case 2:
-            wear = 't';
-            break;
-        default:
-            throw new Error('what?!');
-    }
-
-    return `${color}_${wear}.png`;
+// Create HTML list item from the given data item
+function createHTMLString(item) {
+    return `
+    <li class="item">
+        <img src="${item.image}" alt="${item.type}" class="item__thumbnail">
+        <span class="item__description">${item.gender}, ${item.size}</span>
+    </li>
+    `;
 }
 
-function makeRandomText() {
-    let sex = getRandom(2) === 0 ? 'man' : 'female';
-    let size = getRandom(2) === 0 ? 'large' : 'small';
+function onButtonClick(event, items) {
+    const dataset = event.target.dataset;
+    const key = dataset.key;
+    const value = dataset.value;
 
-    return `${sex}, ${size} size`;
+    if (key === null || value === null) return;
+
+    console.log(items);
+    displayItems(items.filter((item) => item[key] === value));
 }
 
-function getRandom(num) {
-    return Math.floor(Math.random() * (num - 0));
+function setEventListeners(items) {
+    const logo = document.querySelector('.logo');
+    const buttons = document.querySelector('.buttons');
+
+    logo.addEventListener('click', () => displayItems(items));
+
+    buttons.addEventListener('click', (event) => onButtonClick(event, items));
 }
+
+// main
+loadItems()
+    .then((items) => {
+        displayItems(items);
+        setEventListeners(items);
+    })
+    .catch(console.log);
